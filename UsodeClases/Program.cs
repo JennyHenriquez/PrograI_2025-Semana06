19849b71a1f6208a4726501c9c2ecebd17b0f0fe
JennyHenriquez <bh0468032024@unab.edu.sv>
2025-03-11 08:52:53 -0600

    Se suben cambios de Declaracion y Uso de Clases

---
+++ UsodeClases/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsodeClases.Modelos;
+
+namespace UsodeClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Instalar la clase Persona, en otras palabras crear un objeto
+            
+            //Crear el objeto "persona1"
+            Persona persona1 = new Persona("Mario", "Alvarenga", 19, DateTime.Parse("2007/05/02"), "06223410-5", "7023-0000", "San Salvador, Centro", "marioalvarengagmail.com");
+            //Mostrar datos del objeto persona1
+            Console.WriteLine(persona1.MostrarDatos());
+
+            //Crear objeto ""persona2"
+            Persona persona2 = new Persona("Marta", "Guillen", 45, DateTime.Parse("1980/04/06"), "09873456-6", "7677-0480", "Chalatenango, Centro", "martaguillengmail.com");
+            //Mostrar datos del objeto persona2
+            Console.WriteLine(persona2.MostrarDatos());
+
+            Console.ReadLine();
+        }
+    }
+}
